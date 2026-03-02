e3de3102edf5a9da0906744422635a868867166d
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-02 22:12:55 +0200

    feat: rendering footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import NavbarVan from './components/NavbarVan'
 import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
@@ -10,10 +11,12 @@ function App() {
         <NavbarVan/>
         <Routes>
            <Route path='/' element={<Home/>}/>
+           <Route path='about' element={<About/>} />
         </Routes>
        
         
-      
+        <footer className='bg-[#252525] h-20 flex items-center justify-center text-white'> &copy; {new Date().getFullYear()} Bazayesu Didier.#VANLIFE </footer>
+
       </BrowserRouter>
     </>
     
