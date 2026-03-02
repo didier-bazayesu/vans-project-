410c39ed4520155be43bdaec9a1c0fb57e924e95
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-02 19:52:53 +0200

    feat: rendering the whole components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import NavbarVan from './components/NavbarVan'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div className='bg-amber-400'>App</div>
+    <>
+      <BrowserRouter>
+        <NavbarVan/>
+        <Routes>
+           <Route path='/' element={<Home/>}/>
+        </Routes>
+       
+        
+      
+      </BrowserRouter>
+    </>
+    
   )
 }
 
