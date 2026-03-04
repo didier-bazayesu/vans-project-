72631b8fcc7d1ead2b2a98869e0b71394ee00c65
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-04 18:38:40 +0200

    feat: rendering only information about the vans

---
+++ src/pages/host/HostVaninfo.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useOutletContext } from 'react-router-dom'
+
+function HostVaninfo() {
+    let informations = useOutletContext() ;
+    console.log(informations.description);
+    
+    return (
+        <div className="space-y-4 text-[#161616] mt-10">
+            
+            <h4>
+            <span className="font-extrabold">Name: </span>
+            {informations.name}
+            </h4>
+
+            <h4>
+            <span className="font-extrabold">Category: </span>
+            {informations.type}
+            </h4>
+
+            <h4 className="leading-relaxed">
+            <span className="font-extrabold">Description: </span>
+            {informations.description}
+            </h4>
+
+            <h4>
+            <span className="font-extrabold">Visibility: </span>
+            public
+            </h4>
+
+        </div>
+    )
+
+}
+
+export default HostVaninfo
