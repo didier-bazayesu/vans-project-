1d75a57c9510738e2f9224af0325ed644011c0ca
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-04 13:16:28 +0200

    feat: adding footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-[#252525] h-20 flex items-center justify-center text-white'>
+      &copy; {new Date().getFullYear()} Bazayesu Didier.#VANLIFE
+     </footer>
+   )
+}
+
