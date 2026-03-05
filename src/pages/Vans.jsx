8bbf1e3aa9676d92f763c0512463856edd6e3890
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-05 14:13:16 +0200

    feat: Navigating using useNavigation hook

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom" // Don't forget to import this!
+import { Link, NavLink } from "react-router-dom" // Don't forget to import this!
+import { useSearchParams } from "react-router-dom"
 
 export default function Vans() {
     const [vans, setVans] = useState([])
+    const[params , setParams] = useSearchParams();
+    let type = params.get('type')
+    type = type !== null? type.toLowerCase(): null
+
+    
 
     useEffect(() => {
         fetch("/api/vans")
@@ -10,7 +16,7 @@ export default function Vans() {
             .then(data => setVans(data.vans))
     }, [])
 
-    const vanElements = vans.map(van => {
+    const vanElements = vans.filter(element => type? element.type == type : true).map(van => {
         // Handle logic for colors before the return
         const typeClass = van.type === "simple" ? "bg-[#E17654]" : 
                          van.type === "rugged" ? "bg-[#115E59]" : "bg-[#161616]"
@@ -18,7 +24,13 @@ export default function Vans() {
         return (
             <div key={van.id} className="van-tile group">
                 {/* Wrap the content in a Link to make it clickable */}
-                <Link to={`/vans/${van.id}`} className="text-inherit no-underline">
+                <Link 
+                 to={`${van.id}`} 
+                 className="text-inherit no-underline"
+                 state={{search :`?${params.toString()}`}}
+                >
+
+
                     <img 
                         src={van.imageUrl} 
                         className="w-full aspect-square rounded-lg object-cover" 
@@ -42,6 +54,57 @@ export default function Vans() {
     return (
         <div className="p-6">
             <h1 className="text-3xl font-bold mb-8">Explore our van options</h1>
+
+
+            <div className="flex my-5 gap-10">
+            
+                <NavLink
+                    to="?type=simple"
+                    className={
+                    type === "simple"
+                        ? "text-orange-600 underline font-bold "
+                        : "text-blue-500 "
+                    }
+                >
+                    Simple
+                </NavLink>
+
+                <button
+                    onClick={()=> (setParams({type: 'rugged'}))}
+                    className={
+                    type === "rugged"
+                        ? "text-teal-700 underline font-bold"
+                        : "text-blue-500 cursor-pointer"
+                    }
+                >
+                    Rugged
+                </button>
+
+                <button
+                    onClick={()=> (setParams({type: 'luxury'}))}
+                    className={
+                    type === "luxury"
+                        ? "text-black underline font-bold"
+                        : "text-blue-500 cursor-pointer"
+                    }
+                >
+                    Luxury
+                </button>
+
+               { type &&   <button
+                    onClick={()=> (setParams({}))}
+                    className={
+                    type === "rugged"
+                        ? "text-teal-700 underline font-bold"
+                        : "text-blue-500 cursor-pointer"
+                    }
+                >
+                    clear
+                   </button>
+                }
+
+            </div>
+           
             
             <div className="grid grid-cols-3 gap-8">
                 {vanElements}
