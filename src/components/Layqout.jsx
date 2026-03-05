458b87cc26501127711a9e0dff8bd1d01a50d8b7
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-05 14:11:22 +0200

    feat: adding some changes

--- src/components/Layqout.jsx
+++ src/components/Layqout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router-dom'
 import NavbarVan from './NavbarVan'
 import Footer from './Footer'
 
+
 function Layqout() {
   return (
    <div className="flex flex-col min-h-screen">
