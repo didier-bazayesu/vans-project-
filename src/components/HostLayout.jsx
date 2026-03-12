1fa3a6697feee57cb375542b85339255fd04beb2
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-12 16:51:02 +0200

    feat: resolving the problem of protected routes under hosts

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, NavLink, Outlet } from 'react-router-dom'
 
+
 function HostLayout() {
   return (
     <div className="p-6"> 
@@ -8,7 +9,7 @@ function HostLayout() {
           <NavLink to=''className={({isActive}) => isActive ? "text-blue-700 text-shadow-blue-300 underline" : null } end >Dashboard</NavLink>
           <NavLink to='income' className={({isActive}) => isActive ? "text-blue-700 text-shadow-blue-300 underline" : null }>Income</NavLink>
           <NavLink to='vans' className={({isActive}) => isActive ? "text-blue-700 text-shadow-blue-300 underline" : null }>Vans</NavLink>
-          <NavLink to='reviews' className={({isActive}) => isActive ? "text-blue-700 text-shadow-blue-300 underline" : null }>Reviews</NavLink>
+          <NavLink to='reviews' className={({isActive}) => isActive ? "text-blue-700 text-shadow-blue-300 underline" : null } >Reviews</NavLink>
       </nav>
       
       
