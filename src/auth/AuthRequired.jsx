417382398b6322dcf94c35f8c0538fac62a7b983
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-12 18:49:04 +0200

    feat: passing login message under url

--- src/auth/AuthRequired.jsx
+++ src/auth/AuthRequired.jsx
@@ -1,13 +1,13 @@
 import { Outlet, Navigate, useLocation } from "react-router-dom"
 
 export default function AuthRequired() {
-    const isLoggedIn = true
+    const isLoggedIn = false
     const location = useLocation()
-
+     const message = 'You must log in to view this page.'
     if (!isLoggedIn) {
         return (
             <Navigate 
-                to="/login" 
+                to={`/login?message=${encodeURIComponent(message)}`}
                 state={{ message: "You must log in to view this page." }}
                 replace
             />
