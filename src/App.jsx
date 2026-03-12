c721a352e31b6e9f0e2e70c19ba0d89ff5e117a1
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-12 16:48:48 +0200

    feat: rendering all component created

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,25 @@ import NavbarVan from './components/NavbarVan'
 import Home from './pages/Home'
 import About from './pages/About'
 import Vans,{loader as vansLoader} from './pages/Vans'
-import VanDetails from './pages/VanDetails'
-import RoutePage from './pages/RoutePage'
+import VanDetails,{loader as vanDetailsLoader} from './pages/VanDetails'
+
+
 import Layqout from './components/Layout'
 import Income from './pages/host/Income'
 import  Reviews from './pages/host/Reviews'
 import  Dashboard from './pages/host/Dashboard'
-import  HostVans from './pages/host/HostVans'
-import HostVansDetails from './pages/HostVansDetails'
+import  HostVans,{loader as hostVans} from './pages/host/HostVans'
+import HostVansDetails ,{loader as loaderHostVansDetails} from './pages/HostVansDetails'
 import HostLayout from './components/HostLayout'
 import HostVanPricing from './pages/host/HostVanPricing'
 import HostVanPhotos from './pages/host/HostVanPhotos'
 import HostVaninfo from './pages/host/HostVaninfo'
 import NotPagefound from './pages/NotPagefound'
+import Error from './components/Error'
+import Login from './pages/Login'
+import { requireAuth } from './auth/utilis'
+import ProtectedRoute from './components/ProtectedRoute'
+import AuthRequired from './auth/AuthRequired'
 
 
 
@@ -24,30 +30,42 @@ import NotPagefound from './pages/NotPagefound'
 function App() {
 
   const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/'  element={<Layqout/>}>
+    <Route path='/' element={<Layqout />} errorElement={<Error />}>
 
-      <Route path='about' element={<About />} />
-      <Route index element={<Home />} />
-      <Route path='vans' element={<Vans />} loader={vansLoader}  />
-      <Route path='vans/:id' element={<VanDetails />}/>
-      <Route path='vans/:id/Routecomponent' element={<RoutePage/>}></Route>
+  {/* Public routes */}
+  <Route index element={<Home />} />
+  <Route path='about' element={<About />} />
+  <Route path='login' element={<Login />} />
+  
+  {/* Vans routes */}
+  <Route path='vans' element={<Vans />} loader={vansLoader} />
+  <Route 
+    path='vans/:id' 
+    element={<VanDetails />} 
+    loader={vanDetailsLoader}
+  />
+ 
 
-      
-      <Route path="host" element={<HostLayout />}>
+  {/* Host routes */}
+  <Route element={<AuthRequired />}>  // 👈 no path needed
+    <Route path="host" element={<HostLayout />}>
         <Route index element={<Dashboard />} />
-        <Route  path="income" element={<Income />} />
-        <Route path="vans" element={<HostVans />} />
-        <Route path="vans/:id" element={<HostVansDetails />} >
-          <Route path="price" element={<HostVanPricing />} />
-          <Route path="photos" element={<HostVanPhotos/>} />
-          <Route index element={<HostVaninfo/>} />
+        <Route path="income" element={<Income />} />
+        <Route path="vans" element={<HostVans />} loader={hostVans} />
+        <Route path="vans/:id" element={<HostVansDetails />} loader={loaderHostVansDetails}>
+            <Route index element={<HostVaninfo />} />
+            <Route path="price" element={<HostVanPricing />} />
+            <Route path="photos" element={<HostVanPhotos />} />
         </Route>
         <Route path="reviews" element={<Reviews />} />
-      </Route>
-
-
-      <Route path='*'  element={<NotPagefound/>}/>
     </Route>
+</Route>
+
+  {/* Fallback route */}
+  <Route path='*' element={<NotPagefound />} />
+
+</Route>
+
 
   ))
 
