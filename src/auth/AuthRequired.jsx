16dfceb92bfc66b1409a290922b2a3de29274bb2
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-13 14:01:56 +0200

    feat: UseLocation for tracking the expected location of user wanted to login

--- src/auth/AuthRequired.jsx
+++ src/auth/AuthRequired.jsx
@@ -3,11 +3,12 @@ import { Outlet, Navigate, useLocation } from "react-router-dom"
 export default function AuthRequired() {
     const isLoggedIn = localStorage.getItem("loggedIn") === "true"
     const location = useLocation()
+    console.log(location);
 
     if (!isLoggedIn) {
         return (
             <Navigate
-                to={`/login?message=You must log in to view this page.`}
+                to={`/login?message=${encodeURIComponent("You must log in to view this page.")}&redirectTo=${location.pathname}`}
                 state={{ from: location }}
                 replace
             />
