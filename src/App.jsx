0c63e984f7830176848b694d6a8892be8333bac4
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-13 14:05:32 +0200

    feat: Handling UserAuth to access all the routes

--- src/App.jsx
+++ src/App.jsx
@@ -25,63 +25,53 @@ import ProtectedRoute from './components/ProtectedRoute'
 import AuthRequired from './auth/AuthRequired'
 
 
-
-
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Layqout />} errorElement={<Error />}>
 
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path='/' element={<Layqout />} errorElement={<Error />}>
+        {/* Public routes */}
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} loader={loginLoader} action={loginAction} />
 
-  {/* Public routes */}
-  <Route index element={<Home />} />
-  <Route path='about' element={<About />} />
-  <Route path='login' element={<Login />  } loader={loginLoader}  action={ loginAction} />
-  
-  {/* Vans routes */}
-  <Route path='vans' element={<Vans />} loader={vansLoader} />
-  <Route 
-    path='vans/:id' 
-    element={<VanDetails />} 
-    loader={vanDetailsLoader}
-  />
- 
+        {/* Protected routes */}
+        <Route element={<AuthRequired />}>
+          
+          {/* Vans routes */}
+          <Route path="vans" element={<Vans />} loader={vansLoader} />
+          <Route path="vans/:id" element={<VanDetails />} loader={vanDetailsLoader} />
 
-  {/* Host routes */}
-  <Route element={<AuthRequired />}>  // 👈 no path needed
-    <Route path="host" element={<HostLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="income" element={<Income />} />
-        <Route path="vans" element={<HostVans />} loader={hostVans} />
-        <Route path="vans/:id" element={<HostVansDetails />} loader={loaderHostVansDetails}>
-            <Route index element={<HostVaninfo />} />
-            <Route path="price" element={<HostVanPricing />} />
-            <Route path="photos" element={<HostVanPhotos />} />
+          {/* Host routes */}
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="vans" element={<HostVans />} loader={hostVans} />
+            <Route path="vans/:id" element={<HostVansDetails />} loader={loaderHostVansDetails}>
+              <Route index element={<HostVaninfo />} />
+              <Route path="price" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+
         </Route>
-        <Route path="reviews" element={<Reviews />} />
-    </Route>
-</Route>
 
-  {/* Fallback route */}
-  <Route path='*' element={<NotPagefound />} />
+        {/* Fallback route */}
+        <Route path="*" element={<NotPagefound />} />
 
-</Route>
-
-
-  ))
+      </Route>
+    )
+  )
 
   return (
-    
-      
-      <div >        
-        <main className="grow">
-             <RouterProvider router={router} />
-        </main>
-
-       
-
-      </div>
-   
+    <div>
+      <main className="grow">
+        <RouterProvider router={router} />
+      </main>
+    </div>
   )
 }
+
 
 export default App
