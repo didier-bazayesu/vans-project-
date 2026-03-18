d01631b5857be17032a22f0400e83b86c2ca0519
didier-bazayesu <didierbazayesu@gmail.com>
2026-03-18 12:55:43 +0200

    feat: adding helper function that handles the loading state

---
+++ src/help/HelperFunction.jsx
@@ -0,0 +1,7 @@
+ export default function isLoading() {
+
+            return  <div aria-live="assertive" className="flex flex-col items-center justify-center h-64 space-y-4">
+                        <div className="w-12 h-12 border-4 border-gray-200 border-t-[#FF8C38] rounded-full animate-spin"></div>
+                        <p className="text-gray-500 animate-pulse">Loading  all vans...</p>
+                    </div>  
+    }  
